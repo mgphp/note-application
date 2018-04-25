b8927ce3cd70f1df1b5a79b24791d4ff6fac2074
mgphp <mark.grogan1978@gmail.com>
2018-04-25 11:12:46 +0100

    Addition of note taking actions.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,30 @@
-let nextNoteId = 0
-export const addTodo = text => ({
-  type: 'ADD_NOTE',
-  id: nextNoteId++,
-  text
-})
+import types from './actionTypes';
+
+export const addNOTE = text => ({
+  type: types.ADD_NOTE,
+  body,
+});
+
+export const editNOTE = (id, text) => ({
+  type: types.EDIT_NOTE,
+  id,
+  body,
+});
+
+export const removeNOTE = (id, text) => ({
+  type: types.REMOVE_NOTE,
+  id,
+});
+
+export const completeNOTE = id => ({
+  type: types.COMPLETE_NOTE,
+  id,
+});
+
+export const completeAll = id => ({
+  type: types.COMPLETE_ALL,
+});
+
+export const removeCompleted = id => ({
+  type: types.REMOVE_COMPLETED,
+});
